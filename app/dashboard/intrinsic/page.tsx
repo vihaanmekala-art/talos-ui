6aa7b705fa4b12c4114d2e3bd027d155cf78492c
Vihaan Mekala <vihaanmekala@gmail.com>
2026-04-06 20:55:29 -0400

    Added page for intrinic value.

---
+++ app/dashboard/intrinsic/page.tsx
@@ -0,0 +1,8 @@
+export default function Intrinsic() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">💡 Intrinsic Value Calculator</h1>
+            <p className="mt-4 text-gray-400">This service has been removed.</p>
+        </div>
+    )
+}
